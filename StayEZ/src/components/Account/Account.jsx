56ca8467253874e2872530015a21989289c3161a
Desihukka <prashant007.dalal@gmail.com>
2024-08-30 17:54:18 +0530

    Improved Code

--- StayEZ/src/components/Account/Account.jsx
+++ StayEZ/src/components/Account/Account.jsx
@@ -38,8 +38,28 @@ function Account() {
       {subpage === "places" && <Places />}
       {subpage === "bookings" && <Bookings />}
       {subpage === "profile" && (
-        <div>
-          <h2>Hi, {user.name}</h2>
+        <div className="p-4 bg-slate-300">
+          <div className="flex flex-col gap-4 w-3/4 md:w-3/5 text-lg">
+            <h2 className="font-semibold">
+              Hi,{" "}
+              <span className="font-semibold text-rose-500">{user.name}</span>
+            </h2>
+            <p className="text-balance">
+              You can check your bookings by clicking on the "My Bookings" tab
+              above.
+            </p>
+            <p className="text-balance">
+              If you want to list your own place, then Click on "My Places" tab.
+              There you will find "Add New" Button. Clicking it will open a form
+              regarding the details of your place. Just fill the form completely
+              and upload atleast 5 images for better interaction with the
+              customers.
+            </p>
+            <p className="text-balance">
+              Moreover, "My Places" tab will also list your all places, so you
+              can easily keep watch on your listed places.
+            </p>
+          </div>
         </div>
       )}
     </div>
