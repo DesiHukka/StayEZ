1c85f819342d3bf0402cb98b561f834e7d5ef378
Desihukka <prashant007.dalal@gmail.com>
2024-08-30 23:12:42 +0530

    Improved Code

--- StayEZ/src/components/Header/Header.jsx
+++ StayEZ/src/components/Header/Header.jsx
@@ -2,29 +2,11 @@ import { MdOutlineTravelExplore } from "react-icons/md";
 import { BsSearchHeartFill } from "react-icons/bs";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { FaCircleUser } from "react-icons/fa6";
-import { useContext, useState, useEffect } from "react";
+import { useContext } from "react";
 import userContext from "../../context/userContext";
 import { Link } from "react-router-dom";
-import axios from "axios";
 function Header() {
-  // Destination will contain address array for Search functionality.
-  const [destinations, setDestinations] = useState([]);
   const { user } = useContext(userContext);
-  let places;
-  useEffect(() => {
-    const getListings = async () => {
-      const { data } = await axios.get("/");
-      places = data;
-    };
-    getListings();
-  }, []);
-
-  const addresses = places?.map((place) => place.address);
-  if (addresses) {
-    setDestinations([...addresses]);
-    console.log(destinations);
-  }
-  console.log(destinations);
 
   return (
     <header className="flex justify-around my-2 border-b-4 p-2 ">
@@ -34,14 +16,15 @@ function Header() {
           <span className="font-bold text-rose-500 text-lg">StayEZ</span>
         </div>
       </Link>
-      <div className="border-2 sm:border-gray-300 border-rose-400 px-4 py-2 rounded-full sm:shadow-sm shadow-md shadow-rose-400">
-        <div className="bg-white">
-          <ul>
-            {destinations.map((destination) => (
-              <li>{destination}</li>
-            ))}
-          </ul>
-        </div>
+      <div className="flex flex-row  gap-2 border-2 sm:border-gray-300 border-rose-400 px-4 py-2 rounded-full sm:shadow-sm shadow-md shadow-rose-400 items-center">
+        <div>Anywhere</div>
+        <div className="border border-gray-300  h-8"></div>
+        <div>Any week</div>
+        <div className="border border-gray-300  h-8"></div>
+        <div>Add guests</div>
+        <button className="bg-rose-500 p-2 rounded-full text-white">
+          <BsSearchHeartFill />
+        </button>
       </div>
 
       <Link
